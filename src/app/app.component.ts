f91ac3da88482e08cd9f6acf40158a89d0d87ded
Dhananjayahd <dhananjayahd296@gmail.com>
2021-01-29 18:10:38 +0530

    completed with random countries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import arrayWords from '../utils/words';
+import randomArrayWords from '../utils/random';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+
+  words = '';
+  limit = 10;
+  maxRandomWords = 27;
+
+  limitUpdater = (newLimit : number)=>{
+    this.limit = newLimit;
+  }
+
+  generateWords = ()=>{
+    this.words = arrayWords.slice(0,this.limit).join(" ");
+  }
+
+  generateRandomWords = ()=>{
+    let randomArray = [];
+    for(let i=0; i<this.limit; i++){
+      let index = Math.floor(Math.random()*(this.maxRandomWords+1));
+      randomArray.push(randomArrayWords[index]);
+    }
+    this.words = randomArray.join(" ");
+  }
 }
